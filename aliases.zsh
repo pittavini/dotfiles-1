7775b917da7a3e4d6da34365aa37743ac06646a9
pittavini <pittavini@@users.noreply.github.com>
2020-09-30 22:27:23 +0200

    made changes to aliases

--- aliases.zsh
+++ aliases.zsh
@@ -2,40 +2,13 @@
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias reloadshell="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
-alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
-alias sites="cd $HOME/Sites"
-alias lara="sites && cd laravel/"
-alias docs="lara && cd docs/"
-
-# Laravel
-alias a="php artisan"
-alias fresh="php artisan migrate:fresh --seed"
-alias seed="php artisan db:seed"
-
-# PHP
-alias cfresh="rm -rf vendor/ composer.lock && composer i"
-alias php74="docker run -it -w /data -v ${PWD}:/data:delegated --entrypoint php --rm registry.gitlab.com/grahamcampbell/php:7.4"
-alias php8="docker run -it -w /data -v ${PWD}:/data:delegated --entrypoint php --rm registry.gitlab.com/grahamcampbell/php:8.0"
-alias composer="php -d memory_limit=-1 /usr/local/bin/composer"
-
-# JS
-alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
-alias watch="npm run watch"
-
-# Vagrant
-alias v="vagrant global-status"
-alias vup="vagrant up"
-alias vhalt="vagrant halt"
-alias vssh="vagrant ssh"
-alias vreload="vagrant reload"
-alias vrebuild="vagrant destroy --force && vagrant up"
+alias dev="cd $HOME/dev"
 
 # Docker
 alias docker-composer="docker-compose"
@@ -57,3 +30,79 @@ alias resolve="git add . && git commit --no-edit"
 alias stash="git stash -u"
 alias unstage="git restore --staged ."
 alias wip="commit wip"
+
+alias ed="open -a TextEdit"
+alias j14="export JAVA_HOME=`/usr/libexec/java_home -v 14`; java -version"
+alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`; java -version"
+alias tsp="/usr/local/bin/ts"
+alias update="softwareupdate -i -a;bubu;omz update" #brew upgrade --cask --greedy
+alias vpncon="networksetup -connectpppoeservice 'Unibe VPN'"
+alias vpndis="networksetup -disconnectpppoeservice 'Unibe VPN'"
+
+
+#-------- Transmission CLI {{{                                                                                                                                                                                                                                                
+#------------------------------------------------------                                                                                                                                                                                                                       
+# lightweight torrent daemon, has option for cli, webui, ncurses, and gui frontend                                                                                                                                                                                            
+# demo video: http://www.youtube.com/watch?v=ee4XzWuapsE           
+# WebUI:        http://localhost:9091/transmission/web/                                                                                
+#               http://192.168.1.xxx:9091/transmission/web/      
+tsm-clearcompleted() {         
+        transmission-remote -l | grep 100% | grep Done | \
+        awk '{print $1}' | xargs -n 1 -I % transmission-remote -t % -r ;}
+tsm() { transmission-remote --list ;}
+        # numbers of ip being blocked by the blocklist
+        # credit: smw from irc #transmission                       
+tsm-count() { echo "Blocklist rules:" $(curl -s --data \           
+        '{"method": "session-get"}' localhost:9091/transmission/rpc -H \
+        "$(curl -s -D - localhost:9091/transmission/rpc | grep X-Transmission-Session-Id)" \
+        | cut -d: -f 11 | cut -d, -f1) ;}
+# demo video: http://www.youtube.com/watch?v=TyDX50_dC0M
+tsm-blocklist() { $PATH_SCRIPTS/blocklist.sh ;}         # update blocklist
+tsm-daemon() { transmission-daemon ;}                              
+tsm-quit() { pkill -f transmission ;}                              
+tsm-altspeedenable() { transmission-remote --alt-speed ;}       # limit bandwidth
+tsm-altspeeddisable() { transmission-remote --no-alt-speed ;}   # dont limit bandwidth
+tsm-add() { transmission-remote --add "$1" ;}
+tsm-askmorepeers() { transmission-remote -t"$1" --reannounce ;}
+tsm-pause() { transmission-remote -t"$1" --stop ;}              # <id> or all
+tsm-start() { transmission-remote -t"$1" --start ;}             # <id> or all
+tsm-purge() { transmission-remote -t"$1" --remove-and-delete ;} # delete data also
+tsm-remove() { transmission-remote -t"$1" --remove ;}           # leaves data alone
+tsm-info() { transmission-remote -t"$1" --info ;}
+tsm-speed() { while true;do clear; transmission-remote -t"$1" -i | grep Speed;sleep 1;done ;}
+#}}}
+
+
+#-------- surf with elvis {{{
+#------------------------------------------------------
+fzf-surfraw() {
+    search="$(sr -elvis | sed '/^$/d' | sed '/^#/d' | sort -n | fzf -e -i)"
+    echo -n "Enter keyword: "; read keyword
+    surfraw -browser=w3m "$(awk '{print $1}' <<< "$search")" "$keyword"}
+#}}}
+
+#-------- Task-Spooler - Queue Task on The Fly {{{
+#------------------------------------------------------
+# DEMO: https://www.youtube.com/watch?v=jhv-2pNWfr4
+# DESC: switch audio stream to different output (HDMI, Headphone, Speakers ...etc)
+# REFF: http://askubuntu.com/a/18210
+# LINK: http://quvi.sourceforge.net/
+
+alias tsp-ranger='TS_SOCKET=/tmp/ranger tsp'
+alias tsp-notoolrush='TS_SOCKET=/tmp/notoolrush tsp'
+alias tsp-shorttask='TS_SOCKET=/tmp/shorttask tsp'
+alias tsp-mediumtask='TS_SOCKET=/tmp/mediumtask tsp'
+alias tsp-w3m='TS_SOCKET=/tmp/w3m tsp'
+# }}}
+
+#-------- Docker Stuff {{{
+#------------------------------------------------------
+dcleanup() {
+                echo "--------------- Delete Containers -----------------"
+                docker rm $(docker ps -a -q)
+
+                echo "--------------- Delete Images -----------------"
+                docker rmi -f $(docker images -q)
+                echo "---------------------------------------------------"
+}
+#}}}
